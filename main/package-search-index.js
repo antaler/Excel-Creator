b59e3eef733763b9a1504cca714ccb7482ad0e7a
antaler <antaler.dev@outlook.com>
2024-02-23 21:32:56 +0000

    Deploying to javadoc from @ antaler/excel-creator@fdd08e326773373160a2c1cf5bbf5ae1ae505e30 🚀

---
+++ main/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.antaler.utils.excel"},{"l":"com.antaler.utils.excel.annotations"},{"l":"com.antaler.utils.excel.enums"},{"l":"com.antaler.utils.excel.interfaces"},{"l":"com.antaler.utils.excel.styles"}];updateSearchResults();
